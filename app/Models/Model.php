b44371c0c6f712fddeb4c1bb5647ebc99643bd4c
nomantufail <nomantufail100@gmail.com>
2016-11-29 18:48:27 +0500

    comited

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -14,7 +14,6 @@ abstract class Model extends EloquentModel
     public abstract function tableDefinition();
     public function setRawAttributes(array $attributes, $sync = false)
     {
-        dd($attributes);
         foreach($attributes as $key=>$value){
             if(isset($this->$key)){
                 $this->$key = $value;
@@ -34,7 +33,13 @@ abstract class Model extends EloquentModel
                 $this->attributes[$field] = $this->$field;
             }
         }
-        return parent::save();
+        parent::save();
+        foreach($this->attributes as $key=>$value){
+            if(isset($this->$key)){
+                $this->$key = $value;
+            }
+        }
+        return true;
     }
 
     public function fields()
