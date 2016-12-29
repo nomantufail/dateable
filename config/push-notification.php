a5ee2c8228d72c817d9de133f59b05b37817e219
nomantufail <nomantufail@gmail.com>
2016-12-29 12:16:03 +0500

    comited

---
+++ config/push-notification.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'appNameIOS'     => array(
+        'environment' =>'development',
+        'certificate' =>'/path/to/certificate.pem',
+        'passPhrase'  =>'password',
+        'service'     =>'apns'
+    ),
+    'appNameAndroid' => array(
+        'environment' =>'production',
+        'apiKey'      =>'AIzaSyDQ3RXnqBB6vHJ6vZ0pNWm75Um56PPbNJE',
+        'service'     =>'gcm'
+    )
+
+);
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,7 @@ use Illuminate\Http\File;
 */
 
 Route::get('/', function () {
-
+    dd(PushNotification::app('appNameAndroid')
+        ->to("eeWU04zWBWM:APA91bF1FWNHtZFjc0kY7kq-LbrnVpPH-4VGPtSs_l5zgxzfHFlrW1N3m_Fm7emPPuTc6gToFPYLpsF2d4OxHEqGVMWXZRRngpL3X1m-Lkry8bf4X42RwlrkNsOtCmVx2B4n3ZM4hjlM")
+        ->send('fuck you agha'));
 });
