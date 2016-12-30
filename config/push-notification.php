898d12b574954a413eb182e5a791a1e2bc961bf3
waqas ahmad <waqasahmad.codingpixel@gmail.com>
2016-12-30 07:07:21 -0800

    comitd

--- config/push-notification.php
+++ config/push-notification.php
@@ -2,13 +2,13 @@
 
 return array(
 
-    'appNameIOS'     => array(
+    'ios' => array(
         'environment' =>'development',
         'certificate' =>storage_path('app/pem/dateablePush.pem'),
         'passPhrase'  =>'dateable',
         'service'     =>'apns'
     ),
-    'appNameAndroid' => array(
+    'android' => array(
         'environment' =>'production',
         'apiKey'      =>'AIzaSyDQ3RXnqBB6vHJ6vZ0pNWm75Um56PPbNJE',
         'service'     =>'gcm'
